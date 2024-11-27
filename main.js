eddcfe543753c994a7e169a9af36f35be0679148
LionTV <leon.pfeiffer7575@gmail.com>
2024-11-27 09:52:01 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron')
+
+const devMode = true
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    if (devMode) {
+        win.loadURL('http://localhost:5173');
+    } else {
+        win.loadFile('web/dist/index.html');
+    }
+    
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
---
+++ web/src/App.svelte
@@ -0,0 +1,5 @@
+<script>
+    import './app.css'
+</script>
+
+<h1 class="bg-neutral-500">Hallo Welt</h1>
